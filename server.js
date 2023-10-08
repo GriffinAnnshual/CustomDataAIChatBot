1a3677f9fdfc6fb1703116fe98a72c85cd4137df
Griffin Annshual <griffintbr@gmail.com>
2023-10-08 15:28:49 +0530

    Complete Supabase Backend logics

--- server.js
+++ server.js
@@ -1,22 +1,69 @@
 import { createClient } from "@supabase/supabase-js"
-import {Configuration, OpenAIApi} from "openai"
-import dotenv from "dotenv"
-dotenv.config();
+import OpenAI from "openai"
+import dotenv from 'dotenv'
+dotenv.config()
 
-// Initializing the supabase client ==> project_url and api key
-
-const supabase = createClient(
+// Initialize our Supabase client
+console.log(process.env.Supabase_Project_URL)
+const supabaseClient = createClient(
 	process.env.Supabase_Project_URL,
 	process.env.Supabase_API_Key
 )
 
+// generateEmbeddings
+async function generateEmbeddings() {
+	// Initialize OpenAI API
+	const openai = new OpenAI({
+		apiKey: process.env.openai_key,
+	})
 
-//generate Embeddings
+	//Custom data
+	const documents = ["Griffin Annshual is a 18y/o teen who loves programming.",
+	"Griffin's favourite food is Briyani",
+	"Griffin's favourite sport is cricket.",
+	"Griffin is currently pursuing his Computer Science Degree"]
 
-async function generateEmbeddings(){
-    const Configuration = new Configuration({
-			apikey: process.env.Openai_ApiKey,
+
+	
+	for (const document of documents) {
+		const input = document.replace(/\n/g, "")
+		console.log("This is the input given:"+input)
+		console.log("This is the openai key:" + process.env.openai_key)
+		const embeddingResponse = await openai.embeddings.create({
+			model: "text-embedding-ada-002", // Model that creates our embeddings
+			input,
 		})
+
+		console.log(typeof embeddingResponse["data"][0]["embedding"])
+		const list = embeddingResponse["data"][0]["embedding"]
+		const embedding = `[${list.join(",")}]`
+		// Store the embedding and the text in our supabase DB
+      const { data, error } = await supabaseClient.from("documents").insert(
+				{
+					content: document,
+					embedding
+				},
+			)
+			if (error) {
+				console.error("Error inserting data into Supabase:", error)
+			}
+			return
+	}
 }
 
+
+
+async function askQuestion() {
+	const { data, error } = await supabaseClient.functions.invoke(
+		"ask-custom-data",
+		{
+			body: JSON.stringify({ query: "What is Griffin's favourite sport?" }),
+		}
+	)
+	if (error) throw error
+	else{	console.log(data)}
+
+}
+
+askQuestion()
 
